045c96b0b724229f99f26f494213ff0739146ccf
yussan <main.yussan@gmail.com>
2023-04-12 10:43:13 +0700

    feat(add routing on free-practice): ready

--- _free-practice/tanstack-query-vue/src/main.js
+++ _free-practice/tanstack-query-vue/src/main.js
@@ -1,10 +1,21 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import { VueQueryPlugin } from '@tanstack/vue-query'
+import HomePage from './pages/index/index.vue'
 
 import './assets/main.css'
 
+const routes = [{ path: '/', component: HomePage }]
+
+const router = createRouter({
+  mode: 'history',
+  history: createWebHashHistory(),
+  routes // short for `routes: routes`
+})
+
 const app = createApp(App)
 app.use(VueQueryPlugin)
+app.use(router)
 
 app.mount('#app')
